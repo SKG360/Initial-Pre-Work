5c0a451d6a3e1362bf4a0ae0ca7f6739b2d3b438
SKG360 <samuel.giles@colorado.edu>
2019-06-24 16:13:50 -0600

    Adds variable values.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/3-Storing-Values-with-the-Assignment-Operator.js
@@ -0,0 +1,10 @@
+// Summary Comment - Assignment always goes from right to left. Everything to the right of the = operator
+// is resolved before the value is assigned to the variable to the left of the operator.
+
+// Setup
+var a;
+var b = 2;
+
+// Only change code below this line
+a = 7;
+b = a; 
