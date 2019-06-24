94be7c04f3d49bef9b994dbc24e25f80ff829234
SKG360 <samuel.giles@colorado.edu>
2019-06-24 16:31:34 -0600

    Adds camelCase to variables

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -0,0 +1,12 @@
+// Summary Comment - In JavaScript all variables and function names are case sensitive,
+// meaning that capitalization matters.
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
