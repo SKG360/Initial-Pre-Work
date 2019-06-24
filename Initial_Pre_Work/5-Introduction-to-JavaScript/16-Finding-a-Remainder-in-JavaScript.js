d75a13d84dc378d5856df93281d3fa2d57d0da1a
SKG360 <samuel.giles@colorado.edu>
2019-06-24 17:07:15 -0600

    Sets modulo operator to assign remainder to variable.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/16-Finding-a-Remainder-in-JavaScript.js
@@ -0,0 +1,7 @@
+// Summary Comment - The remainder operator % gives the remainder of the division of two numbers.
+//
+// a number can be checked to be even or odd by checking the remainder of the division of the number by 2
+
+// Only change code below this line
+
+var remainder = 11 % 3;
