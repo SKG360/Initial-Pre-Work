581f30aa0e610f8d4b9d8415ccddb248b886782f
SKG360 <samuel.giles@colorado.edu>
2019-06-24 16:06:01 -0600

    Adds myName variable to file.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -0,0 +1,7 @@
+// Suumary Comment - JavaScript provides seven different data types which are
+// undefined, null, boolean, string, symbol, number, and object.
+
+// Example
+var ourName;
+// Declare myName below this line
+var myName;
