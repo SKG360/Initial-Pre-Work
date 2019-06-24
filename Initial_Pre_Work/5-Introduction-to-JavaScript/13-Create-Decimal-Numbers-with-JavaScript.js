747070e7396bfed704df6338113f7aafdbc2d27b
SKG360 <samuel.giles@colorado.edu>
2019-06-24 16:53:42 -0600

    Adds floating point decimal to file.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/13-Create-Decimal-Numbers-with-JavaScript.js
@@ -0,0 +1,7 @@
+// Summary Comment - Decimal numbers are sometimes referred to as floating point numbers or floats.
+
+var ourDecimal = 5.7;
+
+// Only change code below this line
+
+var myDecimal = 5.2;
