a430fc75910a7d47195dd2eddc223ed388e8db50
SKG360 <samuel.giles@colorado.edu>
2019-06-24 16:22:19 -0600

    Initializes three variables.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,12 @@
+// Summary Comment - NaN which means "Not a Number"
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
