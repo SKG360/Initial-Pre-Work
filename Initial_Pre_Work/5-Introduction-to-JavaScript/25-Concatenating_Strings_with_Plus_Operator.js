a152fedc3c29b56d831118fb58b10e17a5026986
SKG360 <samuel.giles@colorado.edu>
2019-06-25 10:48:01 -0600

    Concatenates two strings to file.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/25-Concatenating_Strings_with_Plus_Operator.js
@@ -0,0 +1,8 @@
+// Summary Comment - when the + operator is used with a String value, it is called the concatenation
+
+// Example
+var ourStr = "I come first. " + "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the start. " + "This is the end.";
