acee70620ce742ea30d87f38dfe07560b466b625
SKG360 <samuel.giles@colorado.edu>
2019-06-25 16:13:29 -0600

    Changes immutable string value.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/31-Understand_String_Immutability.js
@@ -0,0 +1,9 @@
+// Summary Comment - In JavaScript, String values are immutable, which means that
+// they cannot be altered once created.
+
+// Setup
+var myStr = "Jello World";
+
+// Only change code below this line
+
+myStr = "Hello World"; // Fix Me
