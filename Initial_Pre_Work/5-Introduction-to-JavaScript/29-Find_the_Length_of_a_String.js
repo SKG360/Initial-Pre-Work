9f536e974b433ffa01ba44eb1b561530839e7acc
SKG360 <samuel.giles@colorado.edu>
2019-06-25 16:02:43 -0600

    Calculates the length of a string.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -0,0 +1,16 @@
+// Summary Comment - the length of a String value can be determined by writing .length
+// after the string variable or string literal.
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
