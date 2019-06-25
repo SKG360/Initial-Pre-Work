50c4abc437f48517edf5f63fe3053aa694c1e6d9
SKG360 <samuel.giles@colorado.edu>
2019-06-25 16:09:47 -0600

    Adds bracket notation to identify first char.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,16 @@
+// Summary Comment - Bracket notation is a way to get a character at a specific index within a string.
+
+// Most modern programming languages start counting at 0. This is referred to as Zero-based indexing.
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
