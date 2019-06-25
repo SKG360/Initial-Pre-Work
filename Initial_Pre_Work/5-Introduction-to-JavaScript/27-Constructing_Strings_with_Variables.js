7ac79ee2e5c71611902d1323175c637491ef47e3
SKG360 <samuel.giles@colorado.edu>
2019-06-25 15:46:59 -0600

    Adds concatenated statement to file.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,10 @@
+// Summary Comment - the concatenation operator (+), can be used to insert one or more
+// variables into a string.
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Kilowatt the Godbody";
+var myStr = "My name is " + myName + " and I am well!"
