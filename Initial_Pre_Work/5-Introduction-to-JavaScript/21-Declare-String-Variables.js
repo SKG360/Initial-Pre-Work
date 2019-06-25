703c82a9479abfdae16674492376fd047e01e253
SKG360 <samuel.giles@colorado.edu>
2019-06-25 09:58:32 -0600

    Adds first and last name strings to file.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,10 @@
+// Summary Comment - a string literal is enclosed in single or double quotes.
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+
+var myFirstName = "Samuel"
+var myLastName = "Giles"
