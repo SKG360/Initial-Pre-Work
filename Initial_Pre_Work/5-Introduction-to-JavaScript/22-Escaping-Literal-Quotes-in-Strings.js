45d1cd0105a0d1f1735f7ca6ca6fdf7c273abf81
SKG360 <samuel.giles@colorado.edu>
2019-06-25 10:19:19 -0600

    Adds backslashes to escape literal quotes.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -0,0 +1,4 @@
+// Summary Comment -  placing a backslash (\) in front of the quote escapes the quote
+// from being considered as the end of the string.
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
