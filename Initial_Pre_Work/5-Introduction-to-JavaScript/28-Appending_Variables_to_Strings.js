7a401ca2b356dc4f8b0292506c5449950e42627c
SKG360 <samuel.giles@colorado.edu>
2019-06-25 15:54:36 -0600

    Adds concatenated string to file.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/28-Appending_Variables_to_Strings.js
@@ -0,0 +1,13 @@
+// Summary Comment - Can append variables to a string using the plus equals (+=) operator.
+
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "challenging";
+var myStr = "Learning to code is ";
+
+myStr += someAdjective
