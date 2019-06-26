7cb080e377e78a7251616428a760ef61ced013df
SKG360 <samuel.giles@colorado.edu>
2019-06-26 08:34:42 -0600

    Adds grocery list of items and quantities.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/45-Shopping_List.js
@@ -0,0 +1,5 @@
+// Summary Comment - The list should be a multi-dimensional array containing several sub-arrays.
+
+var myList = [];
+myList = [["Chocolate Bar", 15], ["Yogurt", 5], ["cheese", 7], ["eggs", 12], ["oatmeal", 3]]
+console.log(myList)
