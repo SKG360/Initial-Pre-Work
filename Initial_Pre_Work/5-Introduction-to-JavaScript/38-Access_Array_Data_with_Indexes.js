ee55bcf5aa1d91aea786a581a3d5516187bfaf1e
SKG360 <samuel.giles@colorado.edu>
2019-06-25 22:16:23 -0600

    Adds first value from array.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -0,0 +1,10 @@
+// Summary Comment - Array indexes are written in the same bracket notation that strings use.
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+var myData = myArray[0];
+// Only change code below this line.
