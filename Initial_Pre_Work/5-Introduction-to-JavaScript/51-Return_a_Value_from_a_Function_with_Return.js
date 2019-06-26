b0b0f6ce980d77cfd36ad7c80192024ba0b24ccc
SKG360 <samuel.giles@colorado.edu>
2019-06-26 09:31:47 -0600

    Adds function to multiply by 5.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,16 @@
+// Summary Comment - Values can pass into a function with arguments. A return statement
+// can be used to send a value back out of a function.
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(num) {
+  return num * 5;
+}
+var newValue = timesFive(3);
+
+console.log(newValue);
+console.log(minusSeven(10));
