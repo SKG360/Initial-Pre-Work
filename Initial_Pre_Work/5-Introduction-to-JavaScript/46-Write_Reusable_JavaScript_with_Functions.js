9a6b9b372d362109fc699f2f3709c7a003eb2311
SKG360 <samuel.giles@colorado.edu>
2019-06-26 08:41:31 -0600

    Adds reusable function to file.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,15 @@
+// Summary Comment - All of the code between the curly braces will be executed
+// every time the function is called.
+
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction() {
+    console.log("Hi World");
+}
+reusableFunction();
