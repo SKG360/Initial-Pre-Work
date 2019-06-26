75c18de601cc902d58c23661bcb7891963b83581
SKG360 <samuel.giles@colorado.edu>
2019-06-26 08:10:00 -0600

    Selects 8 from an array of arrays.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/40-Access_Multi-Dimensional_Arrays_With_Indexes.js
@@ -0,0 +1,7 @@
+// Summary Comment - think of a multi-dimensional array, is as an array of arrays
+
+// Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
