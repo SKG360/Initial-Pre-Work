8b50bb06f9ff9dfcd039dc288c454547569e2108
SKG360 <samuel.giles@colorado.edu>
2019-06-25 22:08:38 -0600

    Adds string and number to array.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/36-Store_Multiple_Values_in_one_Variable_using_JavaScript_Arrays.js
@@ -0,0 +1,8 @@
+// Summary Comment - With JavaScript array variables, several pieces of data can be stored in one place.
+
+
+// Example
+var ourArray = ["John", 23];
+
+// Only change code below this line.
+var myArray = ["kilowatt", 34];
