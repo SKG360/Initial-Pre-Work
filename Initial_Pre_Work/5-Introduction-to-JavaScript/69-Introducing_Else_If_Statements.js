db130fbb39767e39e59275f2096676eb54445e8e
SKG360 <samuel.giles@colorado.edu>
2019-06-26 13:54:14 -0600

    Adds else and else if functions to file.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/69-Introducing_Else_If_Statements.js
@@ -0,0 +1,16 @@
+// Summary Comment - Used to chain multiple conditions together.
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+  else if (val < 5) {
+    return "Smaller than 5";
+  }
+  else {
+    return "Between 5 and 10";
+  }
+}
+// Change this value to test
+
+console.log(testElseIf(7));
