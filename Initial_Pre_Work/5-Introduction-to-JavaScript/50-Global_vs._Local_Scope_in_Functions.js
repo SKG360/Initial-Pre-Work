9707090abac8de875ae0aa9122de6894562ed442
SKG360 <samuel.giles@colorado.edu>
2019-06-26 09:25:54 -0600

    Adds local variable to file.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,16 @@
+// Summary Comment - It is possible to have both local and global variables with the same name.
+// When this occurs, the local variable takes precedence over the global variable.
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
