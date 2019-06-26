641613127031f798e9c656b01a59e0a0d1980620
SKG360 <samuel.giles@colorado.edu>
2019-06-26 08:48:04 -0600

    Passes values thru function.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -0,0 +1,17 @@
+// Summary Comment - Parameters are variables that act as placeholders for the values
+// that are to be input to a function when it is called.  The actual values that are
+// input (or "passed") into a function when it is called are known as arguments.
+
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+
+function functionWithArgs(x, y){
+  console.log(x+y);
+}
+functionWithArgs(3, 8);
