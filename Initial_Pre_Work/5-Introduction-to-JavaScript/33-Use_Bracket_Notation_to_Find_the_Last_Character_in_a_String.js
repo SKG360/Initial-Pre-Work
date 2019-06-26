520f4f1bb81459bb04981f4ff3b5766bd2f06fab
SKG360 <samuel.giles@colorado.edu>
2019-06-25 21:24:50 -0600

    Adds last char from last name with bracket notation.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+// Summary Comment - In order to get the last letter of a string, subtract one from the string's length.
+
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
