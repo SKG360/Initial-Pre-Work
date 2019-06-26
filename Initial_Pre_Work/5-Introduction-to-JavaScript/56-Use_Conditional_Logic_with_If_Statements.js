f6c3bd2a301d6bcc69f3f85ec087f47e46a36f05
SKG360 <samuel.giles@colorado.edu>
2019-06-26 12:11:17 -0600

    Adds boolean variable to file.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,27 @@
+// Summary Comment - The keyword if tells JavaScript to execute the code in the curly
+// braces under certain conditions, defined in the parentheses. These conditions are
+// known as Boolean conditions and they may only be true or false.
+
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+
+  // Only change code below this line.
+  if (wasThatTrue) {
+      return "Yes, that was true"
+  }
+  return "No, that was false"
+
+  // Only change code above this line.
+
+}
+
+// Change this value to test
+trueOrFalse(false);
