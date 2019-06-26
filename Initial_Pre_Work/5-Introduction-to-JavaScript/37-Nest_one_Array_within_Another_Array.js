25f9e23772aa3d95d9a8f4c0cb1f8084a063ebee
SKG360 <samuel.giles@colorado.edu>
2019-06-25 22:13:40 -0600

    Adds nested array.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -0,0 +1,8 @@
+// Summary Comment - a Multi-dimensional Array nests arrays within other arrays.
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["ghost", 62], ["styles", 9]];
+ 
