58fbcfeef9042e2f520bfcc293e649f64611bee2
SKG360 <samuel.giles@colorado.edu>
2019-06-25 21:29:29 -0600

    Adds second to last char.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/34-Use_Bracket_Notation_to_Find_the_Nth-to-Last_Character_in_a_String.js
@@ -0,0 +1,11 @@
+Summary Comment - use the same principles to find the Nth to last char.  
+
+// Example
+var firstName = "Ada";
+var thirdToLastLetterOfFirstName = firstName[firstName.length - 3];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
