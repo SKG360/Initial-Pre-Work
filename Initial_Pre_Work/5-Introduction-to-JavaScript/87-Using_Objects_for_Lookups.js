faab8d3cc30e4afc71600732dfeac77e50924a8d
SKG360 <samuel.giles@colorado.edu>
2019-06-27 09:01:49 -0600

    Exchanges switch statement for key-value pair object.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/87-Using_Objects_for_Lookups.js
@@ -0,0 +1,43 @@
+// Summary Comment - Objects can be thought of as a key/value storage, like a dictionary. 
+
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+  var lookup = {
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank",
+  }
+  result = lookup[val];
+
+  // switch(val) {
+  //   case "alpha":
+  //     result = "Adams";
+  //     break;
+  //   case "bravo":
+  //     result = "Boston";
+  //     break;
+  //   case "charlie":
+  //     result = "Chicago";
+  //     break;
+  //   case "delta":
+  //     result = "Denver";
+  //     break;
+  //   case "echo":
+  //     result = "Easy";
+  //     break;
+  //   case "foxtrot":
+  //     result = "Frank";
+  // }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+console.log(phoneticLookup("delta"));
