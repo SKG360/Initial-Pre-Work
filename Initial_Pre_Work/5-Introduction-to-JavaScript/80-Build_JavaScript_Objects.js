e3cd31a8c4ec374f42af7d44f5d91be2ec130018
SKG360 <samuel.giles@colorado.edu>
2019-06-27 07:14:55 -0600

    Add myDog object to file.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,19 @@
+// Summary Comment - Objects are useful for storing data in a structured way, and can represent real world objects.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Joey",
+  "legs": 7,
+  "tails": 8,
+  "friends": ["diamonds", "pearls"]
+
+};
