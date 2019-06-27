926696f4655dd09b2e2ba73e889c604299757327
SKG360 <samuel.giles@colorado.edu>
2019-06-26 22:26:50 -0600

    Replaces if-then statement with comparison operator return result.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -0,0 +1,9 @@
+// Summary Comment - return result of comparison operator versus if/then statement.
+
+function isLess(a, b) {
+  // Fix this code
+  return a < b
+}
+
+// Change these values to test
+console.log(isLess(10, 15));
