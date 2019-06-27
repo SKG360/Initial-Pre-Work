e17a80391727918645265fc91e2b53570a5b7143
SKG360 <samuel.giles@colorado.edu>
2019-06-27 09:22:22 -0600

    Adds new album to object.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -0,0 +1,30 @@
+// Summary Comment - JavaScript Object Notation or JSON is a related data interchange
+// format used to store data.
+//
+// You will need to place a comma after every object in the array, unless it is the last object in the array.
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Wiz Khalifa",
+    "title": "Pittsburg",
+    "release_year": 2010,
+    "formats": [
+      "CD",
+      "MP3",
+      "LP"
+    ],
+    "platinum": true
+  }
+];
