d10efb4fdf4380992b0863c16369328742e73bd0
SKG360 <samuel.giles@colorado.edu>
2019-06-28 16:42:32 -0600

    Adds parsed integer to the function.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/104-Use_the_parseInt_Function.js
@@ -0,0 +1,8 @@
+// Summary Comment - The parseInt() function parses a string and returns an integer.
+
+function convertToInteger(str) {
+   return parseInt(str);
+}
+
+convertToInteger("56");
+console.log(convertToInteger("56"));
