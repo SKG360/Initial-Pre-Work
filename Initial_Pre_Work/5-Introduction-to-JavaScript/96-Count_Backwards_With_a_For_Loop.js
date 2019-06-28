df10c019d838795e6eb1e7abb927d0f39d437f19
SKG360 <samuel.giles@colorado.edu>
2019-06-28 09:58:39 -0600

    Add inverse counter to file.

--- Initial_Pre_Work/5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,17 @@
+// Summary Comment - A for loop can also count backwards. In order to count backwards
+// change the initialization, condition, and final-expression.
 
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+// Only change code below this line.
+for (var i = 9; i >= 1; i -= 2) {
+  myArray.push(i);
+}
+console.log(myArray)
