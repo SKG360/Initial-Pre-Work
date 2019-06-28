fc6606814935247ba5a3e28d20ea76226d813b9f
SKG360 <samuel.giles@colorado.edu>
2019-06-28 13:47:12 -0600

    Adds do while loop to execute loop once.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/99-Iterate_with_JavaScript_Do_While_loop.js
@@ -0,0 +1,16 @@
+// Summary Comment - Essentially, a do...while loop ensures that the code inside the loop will run at least once.
+
+
+
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+do {
+  myArray.push(i);
+  i += 1;
+}
+while (i < 5);
+
+console.log(myArray)
