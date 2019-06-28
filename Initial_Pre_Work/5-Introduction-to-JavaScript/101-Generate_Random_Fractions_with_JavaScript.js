02cd185c72911195e60d7efce345662bb772fc67
SKG360 <samuel.giles@colorado.edu>
2019-06-28 16:23:27 -0600

    Adds random number calculation.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -0,0 +1,11 @@
+// Summary Comment - Random numbers are useful for creating random behavior.
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
+console.log(randomFraction);
