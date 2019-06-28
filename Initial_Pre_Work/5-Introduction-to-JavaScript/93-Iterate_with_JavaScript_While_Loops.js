1f11ba2a7cff018e2b937f8f340bd8dd28c4e468
SKG360 <samuel.giles@colorado.edu>
2019-06-28 09:31:29 -0600

    Adds while loop to the file.

---
+++ Initial_Pre_Work/5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -0,0 +1,12 @@
+// Summary Comment - a while loop runs "while" a specified condition is true and
+// stops once that condition is no longer true.
+
+// Setup
+var myArray = [];
+var i = 0;
+while(i < 5) {
+  myArray.push(i);
+  i +=1;
+}
+
+// Only change code below this line.
