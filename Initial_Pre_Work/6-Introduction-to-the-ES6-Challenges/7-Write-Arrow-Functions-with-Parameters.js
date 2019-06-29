bd89d9ed358547b5c49569f22eb8cabebc23fa9b
SKG360 <samuel.giles@colorado.edu>
2019-06-29 15:53:38 -0600

    Adds arrow functions with parameters to file.

---
+++ Initial_Pre_Work/6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,8 @@
+// Summary Comment - more than one argument can be passed into arrow functions as well.
+
+const myConcat = (arr1, arr2) => {
+  "use strict";
+  return arr1.concat(arr2);
+};
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
