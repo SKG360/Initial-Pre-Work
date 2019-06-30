1d7b642afb0e464ac805b30630335cb442096f73
SKG360 <samuel.giles@colorado.edu>
2019-06-30 17:02:37 -0600

    Adds import as method to function.

---
+++ Initial_Pre_Work/6-Introduction-to-the-ES6-Challenges/24-Use-Asterisk-to-Import-Everything-from-a-File.js
@@ -0,0 +1,6 @@
+// Summry Comment - use any name following the import * as portion of the statement.
+// In order to utilize this method, it requires an object that receives the imported values.
+// From here, you will use the dot notation to call your imported values.
+
+"use strict";
+import * as theModule from "capitalize_strings";
